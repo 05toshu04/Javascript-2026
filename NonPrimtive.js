2ff10fe6448a04a812c25d5ad25790de289c973a
tushar goswami <tussygoswami01@gmail.com>
2026-09-10 11:00:08 +0530

    Non Primitive Data Types

---
+++ NonPrimtive.js
@@ -0,0 +1,14 @@
+let arr=[23,63,47,"toshu"]; //array
+let obj={
+    number:23,
+    name:"toshu"
+} // object
+
+function greet(){
+    console.log("hello");
+}
+
+console.log(arr);
+console.log(obj);
+greet();
+
