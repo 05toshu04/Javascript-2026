f3958f59e87a15da091e25cfe52e154df77b2f00
tushar goswami <tussygoswami01@gmail.com>
2026-09-17 10:26:46 +0530

    take inputs in javascript

---
+++ TakeInputInJs.js
@@ -0,0 +1,14 @@
+let name=prompt("enter your name")
+console.log(name); //string
+
+let number=prompt("enter the number")
+number=Number.parseInt(number)
+console.log(number)  //number
+
+let n=prompt("enter the decimal number")
+n=Number.parseInt(n)
+console.log(n)   //float 
+
+
+
+
